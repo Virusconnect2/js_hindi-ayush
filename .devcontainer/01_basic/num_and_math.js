ac21b8916e80d4f0df388494f9936a77cd1518a4
Virusconnect2 <132895941+Virusconnect2@users.noreply.github.com>
2024-04-02 06:28:59 +0000

    num_and _math

---
+++ .devcontainer/01_basic/num_and_math.js
@@ -0,0 +1,9 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+const min = 10 
+const max = 20 
+console.log(Math.floor(Math.random()*(max - min + 1) + min));
