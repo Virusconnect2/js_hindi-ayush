85aff5fb3dbb004615dd299ace1d8924bbdd5422
Virusconnect2 <132895941+Virusconnect2@users.noreply.github.com>
2024-04-03 06:44:30 +0000

    arrey

---
+++ .devcontainer/02_basics/arrey.js
@@ -0,0 +1,8 @@
+// const first_array = [1,2,3,[4,5,6],7,[8,[3,2]]]
+// const real_anotherarrey = first_array.flat(Infinity)
+// console.log(real_anotherarrey);
+
+
+const new_arrey = ['ayush','vashistha']
+const another_arrey = new_arrey.indexOf('vashistha') 
+console.log( another_arrey);
